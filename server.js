5e2ad19beb4e2c56024b23d82e994b592f29dd79
Thom <tomdagz@gmail.com>
2019-05-10 22:00:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const sqlite = require('sqlite3');
+const bodyParser = require('body-parser');
+const sequelize_package = require('sequelize');
+
+const app = express();
+
+app.listen(3000);
